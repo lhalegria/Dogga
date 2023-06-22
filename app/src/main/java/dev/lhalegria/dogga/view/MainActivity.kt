3b5693b03071851bbeaa616da953e92ffea2fa45
Luiz Alegria <lhalegria@users.noreply.github.com>
2023-06-22 20:37:17 -0300

    Feature: Add view layer (#3)

--- app/src/main/java/dev/lhalegria/dogga/view/MainActivity.kt
+++ app/src/main/java/dev/lhalegria/dogga/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.lhalegria.dogga
+package dev.lhalegria.dogga.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.lhalegria.dogga.ui.theme.DoggaTheme
+import dev.lhalegria.dogga.view.ui.theme.DoggaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/dev/lhalegria/dogga/view/ui/theme/Type.kt
+++ app/src/main/java/dev/lhalegria/dogga/view/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package dev.lhalegria.dogga.ui.theme
+package dev.lhalegria.dogga.view.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
